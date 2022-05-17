fcd79dc7c58ae181db8d1fb608a65cca1d345c08
riley <rileywestonmiller@outlook.com>
2022-05-16 17:37:31 -0700

    started wren integration

--- src/main.c
+++ src/main.c
@@ -16,6 +16,7 @@
 // along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 #include <stdio.h>
+#include "wren/wren.h"
 int main(int argc, char* argv[]){
     printf("hello world!");
     return 0;
